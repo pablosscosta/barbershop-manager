78d77e68165f8c39ad3dcb8ddc1fa7421a0caf1c
Pablo S S da Costa <pablo.ufopa@gmail.com>
2025-12-26 08:16:08 -0300

    docs(api): adicionar rotas para schema da API e Swagger UI

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -4,10 +4,13 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include("api.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ]

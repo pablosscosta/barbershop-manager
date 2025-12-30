8165d4cd32c395f95cf588473626508a7ad0aba0
Pablo S S da Costa <pablo.ufopa@gmail.com>
2025-12-30 16:05:18 -0300

    feat(frontend): implementa estrutura inicial do front (react + vite)

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Barbershop Manager</h1>
+      <p>Frontend está rodando 🚀</p>
+    </div>
+  );
+}
+
+export default Home;
